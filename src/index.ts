4346bf4ee9125f37633f0683905fbab9ecc69ffd
Omar Suárez Doro <alu0101483474@ull.edu.es>
2024-08-16 08:04:48 +0200

    Added some actions for SonarCloud and Coverage  tools

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import chalk from 'chalk';
+import { connect } from 'mongoose';
+
+/**
+ * Function to start the database connection
+ * @returns void
+ */
+function startDB() : void {
+  connect(process.env.MONGODB_URL!).then(() => {
+    console.log(chalk.greenBright('[Server_startdb] Connected to the database'));
+  }).catch(() => {
+    console.log(chalk.red('[Server_startdb] Something went wrong when conecting to the database'));
+    process.exit(-1);
+  });
+}
+
+// Initialize the express server
+export const app = express();
+app.use(express.json());
+console.log(chalk.green('[Server_start] Server started!'));
+app.listen(3000);
+startDB();
