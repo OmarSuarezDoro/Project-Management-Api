67579d427dde5277e41329d46cb8b2dffc050cd5
Omar Suárez Doro <91568642+OmarSuarezDoro@users.noreply.github.com>
2024-08-26 21:29:14 +0000

    Pending changes exported from your codespace

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LoginSignup from './LogInSignUp/LogInSignUp'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <LoginSignup />
+      </div>
+    </>
+  )
+}
+
+export default App
